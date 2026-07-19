7e2ea730dad459d85555764743a88940d16a1db7
incendiboschivi-italia <gabriele.vaccaro2509@gmail.com>
2026-07-19 16:59:44 +0200

    Create firebase-config.js

---
+++ assets/firebase-config.js
@@ -0,0 +1,14 @@
+// Configurazione del progetto Firebase.
+// Questi valori NON sono segreti: fanno parte del funzionamento normale di
+// un'app web con Firebase e possono stare tranquillamente in un file pubblico.
+// La vera sicurezza è garantita dalle regole di Firestore (file firestore.rules),
+// non dal nascondere questi valori.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXOG7ezWRtvNnUVpKMHsHK08o9JcQa6vw",
+  authDomain: "pyranet-italia-a31bb.firebaseapp.com",
+  projectId: "pyranet-italia-a31bb",
+  storageBucket: "pyranet-italia-a31bb.firebasestorage.app",
+  messagingSenderId: "487249424872",
+  appId: "1:487249424872:web:7fcfffc3201fd96672f45d",
+};
